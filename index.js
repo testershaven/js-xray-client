a3b0c9920f375034704bbf3d6670a015c47fc948
Manuel Marcatili <manuel.marcatili@heathmont.net>
2022-09-13 16:29:57 +0300

    update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const {Worker} = require("./worker");
 async function reportToXray(options) {
     try {
         let worker = new Worker();
-        worker.checkOptions(options);
+        await worker.checkOptions(options);
         let requestBody = worker.generateRequest(options.resultsFoldern, options.fileName);
 
         const client = new Client(options.host);
@@ -15,7 +15,7 @@ async function reportToXray(options) {
         await client.sendResults(options.project, options.testPlan, requestBody);
 
         if(options.cleanupFilesAfterUpload) {
-            worker.cleanFolder(options.resultsFolder);
+            await worker.cleanFolder(options.resultsFolder);
         }
     } catch (e) {
         console.error('Reports were not uploaded to Xray due error: ', e.message);
